4151937614e9fbec994d0d1f7e11eb5324d71497
domenicatonziello <domenica.tonziello10@gmail.com>
2023-03-08 15:59:04 +0100

    add delete-btn to show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -9,8 +9,13 @@
         <div class="card-body">
             <h5 class="card-title">{{ $project->title }}</h5>
             <p class="card-text"> {{$project->description }}</p>
-            <a href="{{ $project->link_project}}" class="btn btn-primary me-2">Vedi in GitHub</a>
-            <a href="{{route('admin.projects.index')}}" class="btn btn-secondary">Indietro</a>
+            <a href="{{ $project->link_project}}" class="btn btn-primary">Vedi in GitHub</a>
+            <a href="{{route('admin.projects.index')}}" class="btn btn-secondary my-2">Indietro</a>
+            <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST" class="text-end">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="btn btn-danger">Elimina</button>
+            </form>
         </div>
     </div>
 </div>
